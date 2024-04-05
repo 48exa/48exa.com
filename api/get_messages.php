b92682b8904380abd25f0a65a658c25bc5cc2da5
48exa <97523839+48exa@users.noreply.github.com>
2024-04-05 20:17:40 +0200

    Finalized api

--- api/get_messages.php
+++ api/get_messages.php
@@ -1,5 +1,3 @@
-<?php require_once('../boilerplate/header.php'); ?>
-
 <?php
 include '../database/database.php';
 
--- api/send_message.php
+++ api/send_message.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 include '../database/database.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' || $_SERVER['SERVER_NAME'] != 'localhost') {
@@ -9,24 +8,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' || $_SERVER['SERVER_NAME'] != 'localho
 
   // Check if fields are empty
   if (empty($message) || empty($name)) {
-    echo "<script>alert('Please fill out all fields.'); window.location='/48exa.com'</script>";
+    echo "<script>alert('Please fill out all fields.');</script>";
+    echo "<script>window.location.href = '../';</script>";
     exit;
   }
 
   // Check if the message is longer than 50 characters
-  if (strlen($message) > 50) {
-    die("Your message can only be 50 characters long.");
+  if (strlen($message) < 3) {
+    echo "<script>alert('Your message has to be over 3 characters long.');</script>";
+    echo "<script>window.location.href = '../';</script>";
+    exit;
   }
 
   // Check if the username is longer than 15 characters
-  if (strlen($name) > 15) {
-    die("Your username can only be 15 characters long.");
+  if (strlen($name) < 3) {
+    echo "<script>alert('Your username has to be over 3 characters long.');</script>";
+    echo "<script>window.location.href = '../';</script>";
+    exit;
   }
+
+  session_start();
 
   // Check if the user has sent a message in the last 10 seconds
   if (isset($_SESSION['last_message_time']) && $_SESSION['last_message_time'] > time() - 10) {
-    die("You can only send a message every 10 seconds.");
+    echo "<script>alert('You can only send a message every 10 seconds.');</script>";
+    echo "<script>window.location.href = '../';</script>";
+    exit;
   }
+
+  // Set the current time as the last message time
+  $_SESSION['last_message_time'] = time();
 
   // Sanitize input
   $message = htmlspecialchars($message, ENT_QUOTES | ENT_HTML5, 'UTF-8');
